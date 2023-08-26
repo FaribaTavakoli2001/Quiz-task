874e621834e97f54cfee1fd9bf07eca18aee2679
FaribaTavakoli2001 <117892836+FaribaTavakoli2001@users.noreply.github.com>
2023-08-27 01:16:00 +0330

    first-commit

---
+++ JavaSript/App.js
@@ -0,0 +1,70 @@
+const startbtn = document.querySelector('.btn-home');
+const startquez = document.querySelector('.start-quiz');
+const exitbtn = document.querySelector('.exit-btn');
+const nextbtn = document.querySelector('.next-btn');
+const quizsec = document.querySelector('quiz-sec');
+
+
+
+startbtn.addEventListener('click', () => {
+    startquez.classList.add('active');
+});
+
+
+exitbtn.addEventListener('click', () => {
+    startquez.classList.remove('active');
+});
+
+nextbtn.addEventListener('click', () => {
+    // startquez.classList.add('active');
+    // quizsec.classList.add('active');
+    if (questionCount < questions.length - 1) {
+
+        questionCount++;
+        showQuestion(questionCount);
+    } else {
+
+        console.log('complated')
+    }
+});
+
+
+const optionlist = document.querySelector('.option-list');
+let questionCount = 0;
+
+
+function showQuestion(index) {
+    const questiontext = document.querySelector('.question-text');
+    const optiontext = document.querySelector('.option');
+    questiontext.innerHTML = `${questions[index].num}. ${questions[index].question}`;
+
+    let optionTag = `<div class="option">${questions[index].options[0]}</div>
+    <div class="option">${questions[index].options[1]}</div>
+    <div class="option">${questions[index].options[2]}</div>
+    <div class="option">${questions[index].options[3]}</div>`;
+
+    optionlist.innerHTML = optionTag;
+    const option = document.querySelectorAll('.option');
+
+    for (let i = 0; i < option.length; i++) {
+
+        option[i].setAttribute('onClick', 'optionSelected(this)')
+    }
+
+}
+
+function optionSelected(answer) {
+    let userAnswer = answer.innerHTML;
+    // console.log(userAnswer)
+    let currectAnswer = questions[questionCount].answer;
+    // console.log(currectAnswer);
+    if (userAnswer == currectAnswer) {
+        console.log('yes');
+        option.classList.add('currect')
+    } else {
+        console.log('no');
+    }
+
+}
+
+
---
+++ JavaSript/json.js
@@ -0,0 +1,101 @@
+let questions = [
+    {
+        num: 1,
+        question: "whats your  fname?",
+        answer: 'my fname is helen',
+        options: [
+            "A.my fname is helen.",
+            "B.my lname is helen.",
+            "C.my favorit color is pink.",
+            "D.0."
+
+        ]
+    },
+    {
+        num: 2,
+        question: "The teacher asked if ________ to bring our textbooks to class?",
+        answer: 'we had all remembered',
+        options: [
+            "A.all we had remembered",
+            "B.we had all remembered",
+            "C.had all we remembered",
+            "D.had we all remembered"
+
+        ]
+    },
+    {
+        num: 3,
+        question: "whats your name?",
+        answer: 'my name is helen',
+        options: [
+
+        ]
+    },
+    {
+        num: 4,
+        question: "Those apples cost only 1$ _______killo?",
+        answer: 'a',
+        options: [
+            "A.for",
+            "B.a",
+            "C.by",
+            "D.the"
+        ]
+    },
+    {
+        num: 5,
+        question: "I have never had a good ….. for learning poems?",
+        answer: 'memory',
+        options: [
+            "A.influence",
+            "B.knowledge",
+            "C.memory",
+            "D.report"
+
+        ]
+    },
+    {
+        num: 6,
+        question: "Scientists are still …….. into the causes of cancer?",
+        answer: 'researching',
+        options: [
+            "A.reporting",
+            "B.researching",
+            "C.creating",
+            "D.caring",
+        ]
+    },
+    {
+        num: 7,
+        question: "whats your name?",
+        answer: 'my name is helen',
+        options: [
+
+        ]
+    },
+    {
+        num: 8,
+        question: "whats your name?",
+        answer: 'my name is helen',
+        options: [
+
+        ]
+    },
+    // {
+    //     num: 9,
+    //     question: "whats your name?",
+    //     answer: 'my name is helen',
+    //     options: [
+
+    //     ]
+    // },
+    // {
+    //     num: 10,
+    //     question: "whats your name?",
+    //     answer: 'my name is helen',
+    //     options: [
+
+    //     ]
+    // },
+
+]
